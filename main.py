8bfeec63d821f064e5c911dd9282d3383db08090
HctrNvt <169070816+HctrNvt@users.noreply.github.com>
2024-05-07 17:02:09 +0200

    Ajout de la boucle de jeu

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import pygame
+from pygame.locals import *
 
-if (pygame.init() ==False):
-    print ("Erreur")
-pygame.display.set_mode((1024,768))
+if (pygame.init() == False):
+    print ("Erreur la librairie Pygame n'a pas pu être initialisée")
+
+fenetre = pygame.display.set_mode((1024,768))
 clock = pygame.time.Clock()
-input()
+
+# La boucle de jeu
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                exit()
+            if event.key == pygame.K_q:
+                print("Touche Q")
--- monstre.py
+++ monstre.py
@@ -1,6 +1,13 @@
 class Monstre():
-    def __init__(self,pointDeVie,vitesse) -> None:
+    def __init__(self,pointDeVie,vitesse,image) -> None:
         # Les attributs de la classe Monstre
+        self.image = image
+        self.centre = (0,0)
         self.vie = pointDeVie
         self.vitesse = vitesse
+        pass
+    def dessiner(self):
+        
+        pass
+    def deplacer():
         pass
