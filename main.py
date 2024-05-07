c507189b4b2e6e6289bcde070cbf06fd6d6b035e
HctrNvt <169070816+HctrNvt@users.noreply.github.com>
2024-05-07 17:46:01 +0200

    Ajout de fonctions pour le monstre
    je comprend rien

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import pygame
 from pygame.locals import *
-
+# Import des class du projet
+import monstre
 if (pygame.init() == False):
     print ("Erreur la librairie Pygame n'a pas pu être initialisée")
 
 fenetre = pygame.display.set_mode((1024,768))
 clock = pygame.time.Clock()
+
+ennemi = monstre.Monstre(100,10,"assets/Perso.png")
+ennemi.dessiner(fenetre,0,0)
+ennemi.deplacer(fenetre,1000,10)
 
 # La boucle de jeu
 while True:
@@ -19,3 +24,5 @@ while True:
                 exit()
             if event.key == pygame.K_q:
                 print("Touche Q")
+
+    pygame.display.update()
--- monstre.py
+++ monstre.py
@@ -1,13 +1,18 @@
+import pygame
 class Monstre():
     def __init__(self,pointDeVie,vitesse,image) -> None:
         # Les attributs de la classe Monstre
-        self.image = image
-        self.centre = (0,0)
+        self.corps = pygame.image.load(image).convert()
+        self.rect = self.corps.get_rect()
+        self.rect.topleft = (10,10)
         self.vie = pointDeVie
         self.vitesse = vitesse
         pass
-    def dessiner(self):
-        
+
+    def dessiner(self,fenetre,positionX,positionY):
+        fenetre.blit(self.corps,(positionX,positionY))
         pass
-    def deplacer():
+    # ça marche pas
+    def deplacer(self,fenetre,posX,posY):
+        fenetre.blit(self.corps,(positionX,positionY))
         pass
