04e80ae8301a2126a69470a9e5efa45e9793a054
HctrNvt <169070816+HctrNvt@users.noreply.github.com>
2024-05-09 19:21:42 +0200

    Finalisation des scores
    - On peut ajouter une nouvelle ligne
    - On peut récupérer le score sous forme de dictionnaire

--- score.py
+++ score.py
@@ -1,19 +1,36 @@
 import os
+import csv
 
-
+nomFichier = "fichiers/score.csv"
 def initialisationFichier():
-    nom_fichier = "fichiers/score.csv"
-    if os.path.exists(nom_fichier) == False:
+    if os.path.exists(nomFichier) == False:
         try:
-            with open(nom_fichier, 'w') as f:
-                print(f"Le fichier {nom_fichier} a été créé avec succès.")
+            with open(nomFichier, 'w') as f:
+                print(f"Le fichier {nomFichier} a été créé avec succès.")
         except Exception as e:
             print(f"Une erreur s'est produite lors de la création du fichier : {str(e)}")
         return True
     return True
 
 # à faire
-def ajouterScore(pseudo,score):
-    pass
+def ajouterScore(pseudo:str,score:int):
+    try:
+        with open(nomFichier, mode='a', newline='') as fichier_csv:
+            writer = csv.writer(fichier_csv)
+            writer.writerow([pseudo, score])
+        print("Ligne ajoutée avec succès.")
+    except Exception as e:
+        print(f"Une erreur s'est produite lors de l'ajout de la ligne : {str(e)}")
 def recupererScores():
-    pass
+    scores = {}
+    try:
+        with open(nomFichier, mode='r') as fichier_csv:
+            lecteur = csv.reader(fichier_csv)
+            next(lecteur)  # Skip header row if exists
+            for row in lecteur:
+                nom, score = row
+                scores[nom] = int(score)
+        return scores
+    except Exception as e:
+        print(f"Une erreur s'est produite lors de la récupération des scores : {str(e)}")
+        return None
